85319ba50fff6c68cc87f72717b9d2a84b215c45
Yu-Wei Chao <ywchao@umich.edu>
2015-05-14 10:09:01 -0400

    cvpr 2015 preprint

---
+++ upload_script.sh
@@ -0,0 +1,4 @@
+# scp -r index.jemdoc index.html publications.jemdoc publications.html ywchao@sftp.itd.umich.edu:~/Public/html/
+# scp -r pictures/cvpr2015.png                                         ywchao@sftp.itd.umich.edu:~/Public/html/pictures/
+scp -r publications.jemdoc publications.html ywchao@sftp.itd.umich.edu:~/Public/html/
+scp -r publications/chao_cvpr2015.pdf publications/chao_cvpr2015.bib ywchao@sftp.itd.umich.edu:~/Public/html/publications/
